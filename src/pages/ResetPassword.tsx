569fe439665062b224a90a302efa9ed9004a37cd
shadrxcc <akaadesm@gmail.com>
2023-11-23 15:53:35 +0100

    feat: implement reset password pages

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+// import { Link, useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import Card from "../components/Card";
 import PageHeader from "../components/PageHeader";

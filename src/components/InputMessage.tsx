e737e194f7ed6a50c82dd626f2cd08f625f6456b
shadrxcc <akaadesm@gmail.com>
2023-11-24 14:22:37 +0100

    feat: adjust user input

--- src/components/InputMessage.tsx
+++ src/components/InputMessage.tsx
@@ -3,16 +3,16 @@ const InputMessage = () => {
     <div className="space-y-3">
       <div className="flex max-w-[80%] mx-auto">
       <div className="flex flex-1 items-center gap-x-3">
-        <button className="flex items-center py-1.5 px-3 bg-[#EEE] rounded-lg gap-x-2.5">
+        <button className="flex items-center py-1.5 text-sm leading-5 text-[#585858]  px-3 bg-[#EEE] rounded-lg gap-x-2.5">
           <img src="/translate.svg" alt="" /> Translate
         </button>
-        <button className="flex items-center py-1.5 px-3 bg-[#EEE] rounded-lg gap-x-2.5">
+        <button className="flex items-center py-1.5 text-sm leading-5 text-[#585858]  px-3 bg-[#EEE] rounded-lg gap-x-2.5">
           <img src="/stars.svg" alt="" />
           Improve
         </button>
       </div>
 
-      <div className="flex-1"></div>
+      <div className="flex-1 hidden sm:block"></div>
       </div>
       <form className="max-w-[80%] mx-auto">
         <div className="flex items-center mb-9 gap-x-2  rounded-lg w-full bottom-0 m-auto justify-between px-4 py-3 border border-[#E7E7E7]">

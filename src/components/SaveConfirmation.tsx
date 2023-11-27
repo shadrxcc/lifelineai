3e03d2ea6d990c9c7bead32c5ccbaf76cc4cc8f6
shadrxcc <akaadesm@gmail.com>
2023-11-27 23:08:32 +0100

    feat: close modals

--- src/components/SaveConfirmation.tsx
+++ src/components/SaveConfirmation.tsx
@@ -3,35 +3,36 @@ import ModalCard from "./ModalCard";
 
 interface ConfirmProps {
   onClose: () => void;
-
 }
 
-const SaveConfirmation = ({onClose} : ConfirmProps) => {
+const SaveConfirmation = ({ onClose }: ConfirmProps) => {
   return (
     <Modal onClose={onClose}>
       <ModalCard className="gap-y-3" heading="Save Confirmation">
-      <p className="text-[#242424] text-base leading-6">
-        You are about to turn off all reminders previously set. This action can
-        be undone at anytime. Are you sure?
-      </p>
+        <p className="text-[#242424] text-base leading-6">
+          You are about to turn off all reminders previously set. This action
+          can be undone at anytime. Are you sure?
+        </p>
 
-      <div className="flex justify-end w-full">
-        <div className="flex items-center gap-x-2">
-          <button
-            id="agree"
-            className="px-6 rounded-lg text-white text-xs font-semibold leading-4 py-2"
-          >
-            Confirm
-          </button>{" "}
-          <button onClick={onClose}
-            id="cancel"
-            className="px-6 py-2 bg-[#EDF8FF] leading-4 text-xs font-semibold rounded-lg gradient-text text-[#EDF8FF]"
-          >
-            <p id="cancel-text">Cancel</p>
-          </button>
+        <div className="flex justify-end w-full">
+          <div className="flex items-center gap-x-2">
+            <button
+              onClick={onClose}
+              id="agree"
+              className="px-6 rounded-lg text-white text-xs font-semibold leading-4 py-2"
+            >
+              Confirm
+            </button>{" "}
+            <button
+              onClick={onClose}
+              id="cancel"
+              className="px-6 py-2 bg-[#EDF8FF] leading-4 text-xs font-semibold rounded-lg gradient-text text-[#EDF8FF]"
+            >
+              <p id="cancel-text">Cancel</p>
+            </button>
+          </div>
         </div>
-      </div>
-    </ModalCard>
+      </ModalCard>
     </Modal>
   );
 };
--- src/components/TurnOnReminder.tsx
+++ src/components/TurnOnReminder.tsx
@@ -3,34 +3,36 @@ import ModalCard from "./ModalCard";
 
 interface ConfirmProps {
   onClose: () => void;
-
 }
 
-const TurnOnConfirmation = ({onClose} : ConfirmProps) => {
+const TurnOnConfirmation = ({ onClose }: ConfirmProps) => {
   return (
     <Modal onClose={onClose}>
       <ModalCard className="gap-y-3" heading="Confirmation">
-      <p className="text-[#242424] text-base leading-6">
-      You are about to turn on reminders previously. This can be undone at anytime. Are you sure of this action?
-      </p>
+        <p className="text-[#242424] text-base leading-6">
+          You are about to turn on reminders previously. This can be undone at
+          anytime. Are you sure of this action?
+        </p>
 
-      <div className="flex justify-end w-full">
-        <div className="flex items-center gap-x-2">
-          <button
-            id="agree"
-            className="px-6 rounded-lg text-white text-xs font-semibold leading-4 py-2"
-          >
-            Confirm
-          </button>{" "}
-          <button
-            id="cancel" onClick={onClose}
-            className="px-6 py-2 bg-[#EDF8FF] leading-4 text-xs font-semibold rounded-lg gradient-text text-[#EDF8FF]"
-          >
-            <p id="cancel-text">Cancel</p>
-          </button>
+        <div className="flex justify-end w-full">
+          <div className="flex items-center gap-x-2">
+            <button
+              onClick={onClose}
+              id="agree"
+              className="px-6 rounded-lg text-white text-xs font-semibold leading-4 py-2"
+            >
+              Confirm
+            </button>{" "}
+            <button
+              id="cancel"
+              onClick={onClose}
+              className="px-6 py-2 bg-[#EDF8FF] leading-4 text-xs font-semibold rounded-lg gradient-text text-[#EDF8FF]"
+            >
+              <p id="cancel-text">Cancel</p>
+            </button>
+          </div>
         </div>
-      </div>
-    </ModalCard>
+      </ModalCard>
     </Modal>
   );
 };
